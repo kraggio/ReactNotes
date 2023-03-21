4ab09c80a45fb1ab2b1beb09e65efd199cedb727
kraggio <keilyraggio18@gmail.com>
2023-03-21 15:40:40 -0400

    Only need to finish up notes from here, all challenges done

--- NotesS6/notes.js
+++ NotesS6/notes.js
@@ -13,8 +13,58 @@ We will lern about :
 
 --Setting Dynamic Inline Styles
 CODING EXERCISE 11: DYNAMIC STYLES
+
+Your task is to dynamically apply a style (color: red) to the <p>Style me</p> element in the provided React app.
+
+The style should be applied as an inline style (i.e., via the style attribute / prop) when the <button> is clicked for the first time. Once the button is clicked again, the styling should switch back to color: white, which should also be the initial style.
+
+Make sure that the button toggles between these two styles (color: white <=> color: red).
+
+import React from 'react';
+ 
+// don't change the Component name "App"
+export default function App() {
+    const [highlighted, setHighlighted] = React.useState(false);
+    
+    function clickHandler() {
+        setHighlighted(isHighlighted => !isHighlighted);
+    }
+    
+    return (
+        <div>
+            <p style={{color: highlighted ? 'red' : 'white'}}>Style me!</p>
+            <button onClick={clickHandler}>Toggle style</button>
+        </div>
+    );
+}
+
 --Setting CSS Classes Dynamically--
 CODING EXERCISE 12: DYNAMIC CSS CLASSES
+
+Your task is to dynamically apply a CSS class (active) to the <p>Style me</p> element in the provided React app. This task is therefore very similar to the previous coding exercise but you're now going to set a CSS class dynamically - instead of an inline style.
+
+The style should be applied as an CSS class (i.e., via the className attribute / prop) when the <button> is clicked for the first time. Once the button is clicked again, all CSS classes should be removed from the <p> element (this should also be the initial state).
+
+Make sure that the button toggles between these two styles (no CSS class <=> active  CSS class).
+
+import React from 'react';
+ 
+// don't change the Component name "App"
+export default function App() {
+    const [highlighted, setHighlighted] = React.useState(false);
+    
+    function clickHandler() {
+        setHighlighted(isHighlighted => !isHighlighted);
+    }
+    
+    return (
+        <div>
+            <p className={highlighted ? 'active' : ''}>Style me!</p>
+            <button onClick={clickHandler}>Toggle style</button>
+        </div>
+    );
+}
+
 --Introducing Styled Components--
 --Styled Components & Dynamic Props--
 --Styled Components & Media Queries-- 
--- NotesS7/notes.js
+++ NotesS7/notes.js
@@ -2,7 +2,32 @@
 
 --MODULE INTRODUCTION--
 --UNDERSTANDING REACT ERROR MESSAGES--
+
+
 CODING EXERCISE 13: FIX ERRORS
+
+onclick and Div were changed to <div> and onClick
+
+import React from 'react';
+
+import './styles.css';
+
+// don't change the Component name "App"
+export default function App() {
+    const clickHandler = () => {
+        console.log('Clicked!');
+    };
+    
+    return (
+        <div>
+          <h2>You're logged in!</h2>
+          <p>Welcome to your user profile!</p>
+          <button onClick={clickHandler}>Click me!</button>
+        </div>
+    );
+}
+
+
 --ANALYZING CODE FLOW & WARNINGS--
 --WORKING WITH BREAKPOINTS--
 --USING THE REACT DEVTOOLS--
